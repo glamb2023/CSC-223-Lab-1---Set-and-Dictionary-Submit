3aa50abb1ed31b8db0df13490a94b10d6dc33999
taterosen <taterosen@gmail.com>
2022-09-02 12:02:12 -0400

    Header and method comments
    Added comments to source code file PAD and to methods that are not self-explanatory. Also changed keySet method to make less redundant.

--- src/utilities/ParallelArrayDictionary.java
+++ src/utilities/ParallelArrayDictionary.java
@@ -1,6 +1,13 @@
 package utilities;
 
 /**
+ * An implementation of a dictionary of key,value pairs that 
+ * contains unique keys in an ArraySet and their values, which
+ * do not need to be unique, in an ArrayList. Redefines methods
+ * from the Map interface.
+ * 
+ * <p> bugs:
+ * 
  * @author taterosen & georgelamb
  * @date 09/02/2022
  */
@@ -43,12 +50,23 @@ public class ParallelArrayDictionary<Key,Value> implements Map<Key,Value> {
 		return _values.contains(value);
 	}
 
+	/**
+	 * Returns null if given key is null or not in dictionary.
+	 * Else returns value at the given key.
+	 * @param key
+	 */
 	@Override
 	public Value get(Object key) {
 		if (key == null || !_keys.contains(key)) return null;
 		return _values.get(_keys.indexOf(key));
 	}
 
+	/**
+	 * Returns null if key is null or not unique. Else
+	 * adds given key and given value to dictionary and
+	 * returns added value.
+	 * @param key, @param value
+	 */
 	@Override
 	public Value put(Key key, Value value) {
 		if (key == null ||  _keys.contains(key)) return null;
@@ -57,6 +75,12 @@ public class ParallelArrayDictionary<Key,Value> implements Map<Key,Value> {
 		return value;
 	}
 
+	/** 
+	 * Returns null if key is null or not in dictionary.
+	 * Else set value at index of given key to null, then
+	 * return removed value.
+	 * @param key
+	 */
 	@Override
 	public Value remove(Object key) {
 		if (key == null || !_keys.contains(key)) return null;
@@ -65,6 +89,11 @@ public class ParallelArrayDictionary<Key,Value> implements Map<Key,Value> {
 		return removedValue;
 	}
 
+	/**
+	 * Only adds key,value pair from given map if 
+	 * key is unique.
+	 * @param m
+	 */
 	@Override
 	public void putAll(Map<? extends Key, ? extends Value> m) {
 		for(Key k: m.keySet()) {
@@ -83,17 +112,23 @@ public class ParallelArrayDictionary<Key,Value> implements Map<Key,Value> {
 
 	@Override
 	public Set<Key> keySet() {
-		ArraySet<Key> keySet = new ArraySet<Key>();
-		keySet.addAll(_keys);
-		return keySet;
+		return _keys;
 	}
 
+	/**
+	 * Creates and returns a collection that contains
+	 * the dictionary's values.
+	 */
 	@Override
 	public Collection<Value> values() {
 		Collection<Value> valueCol = new ArrayList<Value>(_values);
 		return valueCol;
 	}
 
+	/**
+	 * Creates and returns a set of all key,value entries
+	 * in the dictionary after adding all entries.
+	 */
 	@Override
 	public Set<Entry<Key, Value>> entrySet() {
 		Set<Entry<Key,Value>> entries = new ArraySet<Entry<Key,Value>>();
